88d52c62c9a25e046445f1e9e1c9c4586e8b6ab8
OleksandrKoshmak <OleksandrKoshmak99@gmail.com>
2020-12-12 03:21:31 +0200

    create HibernateUtil and DAO

---
+++ src/main/java/hibernateUtil/HibernateUtil.java
@@ -0,0 +1,40 @@
+package hibernateUtil;
+
+import models.University;
+import models.Specialty;
+import models.Subject;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
+
+    public class HibernateUtil {
+
+        private static SessionFactory sessionFactory;
+
+        public HibernateUtil() {
+        }
+
+        public static SessionFactory getSessionFactory() {
+            if (sessionFactory == null) {
+                try {
+                    Configuration configuration = new Configuration().configure();
+//                configuration.addAnnotatedClass(University.class);
+//                configuration.addAnnotatedClass(Specialty.class);
+//                configuration.addAnnotatedClass(Subject.class);
+                    StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                    sessionFactory = configuration.buildSessionFactory(builder.build());
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return sessionFactory;
+        }
+        public static void shutdown(){
+            getSessionFactory().close();
+        }
+    }
+
