5efeb87d8246a436d9bb45f44bc7615f3bc6c287
OleksandrKoshmak <OleksandrKoshmak99@gmail.com>
2020-12-12 02:59:54 +0200

    create DAO

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,27 @@
-import hibernateUtil.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+//import dao.SpecialtyDao;
+//import dao.SubjectDao;
+//import dao.UniversityDao;
+//import models.Specialty;
+//import models.Subject;
+//import models.University;
+//
+//import java.util.List;
+//
+//public class Main {
+//    public static void main(String[]args){
+//        UniversityDao universityDao = new UniversityDao();
+//        SpecialtyDao specialtyDao = new SpecialtyDao();
+//        SubjectDao subjectDao = new SubjectDao();
+//Specialty crimeLaw = specialtyDao.readSpecialty(2);
+//
+//        University university = universityDao.readUniversity(1);
+//        university.setEmail("Harward@mail.com");
+//        crimeLaw.getUniversityList().add(university);
+//        university.getSpecialtyList().add(crimeLaw);
+//        specialtyDao.updateSpecialty(crimeLaw);
+//        universityDao.updateUniversity(university);
+//
+//
+//    }
+//}
 
-public class Main {
-    public static void main(String[] args) {
-Session session = HibernateUtil.getSessionFactory().openSession();
-Transaction tx = session.beginTransaction();
-tx.commit();
-HibernateUtil.shutdown();
-
-    }
-}
